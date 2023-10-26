d62fd8ffbd125e72f3d2cd873b3fcd78587f17a7
“Saphron” <109723086+denerose@users.noreply.github.com>
2023-10-26 11:29:14 +1100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <main>
+      <div className="editorWindow">
+        <h1>Editor</h1>
+      </div>
+      <div className="previewWindow">
+        <h1>Preview</h1>
+      </div>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/footer.jsx
@@ -0,0 +1,12 @@
+import reactLogo from './assets/react.svg'
+
+function Footer() {
+
+    return (
+        <div id="footerContainer">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+        </div>
+    )
+}
+
+export default Footer
